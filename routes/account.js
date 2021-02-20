aee9a5cb5100efdff36bb81c5543be390aa0d325
vovachebr <vovachebr@mail.ru>
2021-02-20 23:23:38 +0300

    Update description TransactionsPage, update account: clear transactions, add validation to unknown account

--- routes/account.js
+++ routes/account.js
@@ -36,9 +36,11 @@ router.put("/", upload.none(), function(request, response) {
 router.delete("/", upload.none(), function(request, response) {
     const db = low(new FileSync('db.json'));// получение БД
     let accounts = db.get("accounts");// получение списка счетов
+    let transactions = db.get("transactions");// получение списка счетов
     let removingAccount = accounts.find({id: request.body.id}).value();// нахождение нужного удаляемого счёта
     if(removingAccount){// если удаляемый аккаунт существует
         accounts.remove({id: request.body.id}).write();// удалить и перезаписать аккаунт
+        transactions.remove({account_id: request.body.id}).write(); // удалить связанные транзакции и перезаписать
         response.json({success: true});// отправка ответа успешности
     }else{// если аккаунта нету
         response.json({success: false});// отправка ответа неуспешности
@@ -62,6 +64,10 @@ router.get("/:id?", upload.none(), function(request, response) {
 
     if(accountId){
         let currentAccount = db.get("accounts").find({id: accountId}).value();
+        if(!currentAccount){
+          response.json({success: false, error: `Счёт c идентификатором ${accountId} не найден`});
+          return;
+        }
         let currentAccountTransactions = db.get("transactions").filter({account_id: currentAccount.id}).value();
         currentAccount.sum = currentAccountTransactions.reduce((sum, a) => a.type === "EXPENSE" ? sum - a.sum : sum + a.sum, 0);
         response.json({success: true, data: currentAccount});
